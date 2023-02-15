1745dcd839d8ee67eb91c836270717a50aecf1bc
jcommaret@gmail.com <jcommaret@gmail.com>
2023-02-15 22:46:11 +0100

    Layout

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom"
 
+import Header from "../../components/header"
+import Sidebar from "../../components/sidebar"
+
 import "./index.scss"
 
 export default function Layout() {
   return (
-    <div className="wrapper">
-      <p>Layout</p>
-      <div className="container">
+    <>
+      <Header />
+      <Sidebar />
+      <main className="main-container">
         <Outlet />
-      </div>
-    </div>
+      </main>
+    </>
   )
 }
