b9e4f8e9f68a8eadc576fe8ccfa9e057f9200413
jcommaret@gmail.com <jcommaret@gmail.com>
2023-02-22 17:38:47 +0100

    barChart

--- src/components/barchart/index.jsx
+++ src/components/barchart/index.jsx
@@ -1,4 +1,5 @@
 import {
+  ResponsiveContainer,
   BarChart,
   Legend,
   Bar,
@@ -10,14 +11,16 @@ import {
 
 export default function BarChartWithUserData({ data }) {
   return (
-    <BarChart width={760} height={200} data={data}>
-      <Legend layout="vetical" verticalAlign="top" align="right" />
-      <CartesianGrid strokeDasharray="1" vertical={false} />
-      <XAxis dataKey="serie" />
-      <Tooltip dataKey="serie" />
-      <Bar barSize={7} dataKey="calories" fill="#282D30" />
-      <Bar barSize={7} dataKey="weight" fill="#E60000" />
-      <YAxis dataKey="weight" orientation="right" />
-    </BarChart>
+    <ResponsiveContainer width="100%" height={200}>
+      <BarChart width={760} height={200} data={data}>
+        <Legend layout="horizontal" verticalAlign="top" align="center" />
+        <CartesianGrid strokeDasharray="1" vertical={false} />
+        <XAxis dataKey="serie" />
+        <Tooltip dataKey="serie" />
+        <Bar barSize={7} dataKey="calories" fill="#282D30" />
+        <Bar barSize={7} dataKey="weight" fill="#E60000" />
+        <YAxis dataKey="weight" orientation="right" />
+      </BarChart>
+    </ResponsiveContainer>
   )
 }
