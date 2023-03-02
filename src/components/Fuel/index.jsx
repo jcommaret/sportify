125c3a80f7b0dbb26b0f631fee2b870d11b04614
jcommaret@gmail.com <jcommaret@gmail.com>
2023-03-02 19:17:12 +0100

    Fuel

--- src/components/Fuel/index.jsx
+++ src/components/Fuel/index.jsx
@@ -5,7 +5,7 @@ import lipides from "../../assets/icon/fat-icon.png"
 
 export default function Fuel({ data }) {
   const foodItems = [
-    { ratio: data.calorieCount, type: "calories", img: calories },
+    { ratio: data.calorieCount, type: "Calories", img: calories },
     { ratio: data.proteinCount, type: "Proteines", img: proteines },
     { ratio: data.carbohydrateCount, type: "Glucides", img: glucides },
     { ratio: data.lipidCount, type: "Lipides", img: lipides },
