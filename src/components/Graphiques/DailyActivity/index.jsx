f749f5763722914c884a5211ba3a09d2da278089
jcommaret@gmail.com <jcommaret@gmail.com>
2023-03-13 16:41:55 +0100

    Graphiques

--- src/components/Graphiques/DailyActivity/index.jsx
+++ src/components/Graphiques/DailyActivity/index.jsx
@@ -8,11 +8,19 @@ import {
   Tooltip,
   CartesianGrid,
 } from "recharts"
-
+/**
+ * Daily activity component
+ * @component DailyActivity is in charge of displaying calories, and kilograms at the top left section of the dashboard
+ * @param {Props}  data
+ * @param {Object} data.data
+ * @param {Number} data.data.calories Calories Amount
+ * @param {Number} data.data.kilograms kilograms Amount
+ * @exports DailyActivity
+ */
 export default function DailyActivity({ data }) {
   return (
     <ResponsiveContainer width="100%" height={200}>
-      <BarChart width={760} height={200} data={data}>
+      <BarChart width={760} height={100} data={data}>
         <Legend
           layout="horizontal"
           verticalAlign="top"
@@ -21,8 +29,18 @@ export default function DailyActivity({ data }) {
         />
         <CartesianGrid strokeDasharray="1" vertical={false} />
         <Tooltip dataKey="calories" />
-        <Bar barSize={7} dataKey="calories" fill="#282D30" />
-        <Bar barSize={7} dataKey="kilogram" fill="#E60000" />
+        <Bar
+          barSize={7}
+          dataKey="calories"
+          fill="#282D30"
+          radius={[5, 5, 0, 0]}
+        />
+        <Bar
+          barSize={7}
+          dataKey="kilogram"
+          fill="#E60000"
+          radius={[5, 5, 0, 0]}
+        />
         <XAxis dataKey="jourNumber" />
         <YAxis dataKey="kilogram" orientation="right" />
       </BarChart>
